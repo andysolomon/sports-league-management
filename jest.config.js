6661e32ead62f1ddc7bfb82871fd9ca6cc2f0786
Andrew Solomon <andrewsolomon.edu@gmail.com>
2025-06-12 11:01:59 -0400

    Fix Jest configuration and add missing dependency

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,14 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
-        '^@salesforce/apex$': '<rootDir>/force-app/test/jest-mocks/apex',
-        '^@salesforce/schema$': '<rootDir>/force-app/test/jest-mocks/schema',
+        '^@salesforce/apex$': '<rootDir>/sportsmgmt/test/jest-mocks/apex',
+        '^@salesforce/schema$': '<rootDir>/sportsmgmt/test/jest-mocks/schema',
         '^lightning/navigation$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
+            '<rootDir>/sportsmgmt/test/jest-mocks/lightning/navigation',
         '^lightning/platformShowToastEvent$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/platformShowToastEvent',
+            '<rootDir>/sportsmgmt/test/jest-mocks/lightning/platformShowToastEvent',
         '^lightning/messageService$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/messageService'
+            '<rootDir>/sportsmgmt/test/jest-mocks/lightning/messageService'
     },
     setupFiles: ['jest-canvas-mock'],
     testTimeout: 10000,
